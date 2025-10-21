db5c9c1e065ba5a7ecc6de18b4422ed58a510492
Mauricio Quezada <mauriqb2@gmail.com>
2025-10-21 13:18:45 -0400

    creando la conexion a la bd y creando entidad usuario

---
+++ mklite_backend/src/entity/user.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    ci: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    lastname: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+}
--- mklite_backend/src/main.ts
+++ mklite_backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppDataSource } from './data-source';
 
 async function bootstrap() {
+  try {
+    await AppDataSource.initialize()
+  } catch (error) {
+      console.log(error)
+  }
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3005);
 }
 bootstrap();
