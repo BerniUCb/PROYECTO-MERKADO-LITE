738b36d320e55537aa47e727fcf69c5954df7d06
Luiscuac <luiguiandre@gmail.com>
2025-11-11 19:36:54 -0400

    product controller, module, service

---
+++ mklite_backend/src/product/product.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'; 
+import { Producto } from '../entity/product.entity';    
+import { ProductService } from './product.service';
+import { ProductController } from './product.controller';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Producto]) 
+  ],
+  controllers: [ProductController],
+  providers: [ProductService],
+})
+export class ProductModule {}
