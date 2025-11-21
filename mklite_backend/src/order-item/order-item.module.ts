11a1902b0eeeb019bbc9a599411fb0f1ecfc25d5
Mateo Wilson Quispe Aylas <mateo.quispe@ucb.edu.bo>
2025-11-21 08:42:43 -0400

    Support ticket

--- mklite_backend/src/order-item/order-item.module.ts
+++ mklite_backend/src/order-item/order-item.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm'; 
 import { OrderItem } from '../entity/order-item.entity';    
-import { OrderItem } from '../entity/order-item.entity';    
 import { OrderItemService } from './order-item.service';
 import { OrderItemController } from './order-item.controller';
 import { Order } from 'src/entity/order.entity';
