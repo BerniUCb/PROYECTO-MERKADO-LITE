4b0ae49ce0a52c2dddb24b9793ccc115ba71cda5
Bernardo Rios T <bernardo.rios@ucb.edu.bo>
2025-11-22 20:03:58 -0400

    cambios v1

--- mklite_backend/src/data-source.ts
+++ mklite_backend/src/data-source.ts
@@ -14,12 +14,9 @@ export const dataSourceOptions: DataSourceOptions = {
 
  
     entities: [join(__dirname, '**', '*.entity{.js,.ts}')],
-<<<<<<< HEAD
     // LÍNEA CORREGIDA Y MÁS ROBUSTA
     migrations: [__dirname + '/database/migrations/*{.ts,.js}'],
-=======
-    migrations: [join(__dirname, 'database', 'migrations', '*{.ts,.js}')],
->>>>>>> 38f0f8db92cd1e6c5ca1d31bd2470ee0e0488866
+
     
     logging: true,
     subscribers: [],
