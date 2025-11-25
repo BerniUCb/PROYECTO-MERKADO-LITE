946c687b84bfc62276db6f905874f357fa2f1de5
Luiscuac <luiguiandre@gmail.com>
2025-11-25 12:42:31 -0400

    dto order-item

--- mklite_backend/src/order-item/order-item.controller.ts
+++ mklite_backend/src/order-item/order-item.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { OrderItemService } from "./order-item.service";
 import { OrderItem } from "src/entity/order-item.entity";
+import { CreateOrderItemDto } from "./dto/create-order-item.dto";
+import { UpdateOrderItemDto } from "./dto/update-order-item.dto";
 
 
 @Controller('/order-item')
@@ -9,9 +11,9 @@ export class OrderItemController {
     constructor(private readonly orderItemService: OrderItemService) {}
 
     @Post()
-    createOrderItem(@Body() orderItem: OrderItem) {
-        return this.orderItemService.createOrderItem(orderItem);
-    }
+    createOrderItem(@Body() dto: CreateOrderItemDto) {
+    return this.orderItemService.createOrderItem(dto);
+}
 
     @Get()
     getAllOrderItems() {
@@ -29,9 +31,10 @@ export class OrderItemController {
     }
 
     @Put('/:id')
-    updateOrderItem(@Param('id') id: number,  @Body() orderItem: OrderItem) {
-        return this.orderItemService.updateOrderItem(id, orderItem);
-    }
+    updateOrderItem(@Param('id') id: number, @Body() dto: UpdateOrderItemDto) {
+    return this.orderItemService.updateOrderItem(id, dto);
+}
+
 
 
 }
