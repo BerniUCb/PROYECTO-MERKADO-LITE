7b1b6ac49d5c3d7ab2802c6adff76533124ef1e0
Igor Pereira <pereirarosadoigor@gmail.com>
2025-11-26 00:27:29 -0400

    Adding details to every page

--- mklite_frontend/app/utils/axios.tsx
+++ mklite_frontend/app/utils/axios.tsx
@@ -4,7 +4,6 @@ export const instance = axios.create({
   baseURL: "http://localhost:3005",
 });
 
-// ⚠️ SOLO agregar el interceptor si estamos en el navegador
 if (typeof window !== "undefined") {
   instance.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
