50841be754700eecd0222c884add9ee769689696
rcamira <rc.amira.rocio@gmail.com>
2025-11-25 20:08:17 -0400

    Modificacion para auth en user

--- mklite_backend/src/auth/auth.module.ts
+++ mklite_backend/src/auth/auth.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { JwtStrategy } from './jwt.strategy';
+
 import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AuthController],
-  providers: [AuthService],
-})
+  imports:[
+    JwtModule.register({
+      secret: 'Secure user',
+      signOptions: { expiresIn: '1h',}
+    }),
+    UserModule,
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 
-export class AuthModule {}
+})
+export class AuthModule{}
